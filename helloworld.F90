5754a72794d1c66f484ba9ed8a48ddaa0c39bcd9
jayeshkrishna <jayesh@mcs.anl.gov>
2014-08-07 22:16:22 -0500

    Printing MPI rank and size in the lib function
    Calling hello() now also prints rank and size of the comm world

--- helloworld.F90
+++ helloworld.F90
@@ -1,3 +1,13 @@
 SUBROUTINE hello()
-  PRINT *, "Hello world"
+  USE mpi
+  
+  INTEGER sz, rank
+  INTEGER ierr
+
+!  call MPI_INIT(ierr)
+  call MPI_COMM_SIZE(MPI_COMM_WORLD, sz, ierr) 
+  call MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierr)
+
+  PRINT *, "Hello world from ", rank, " of ", sz, " processes"
+!  call MPI_FINALIZE(ierr)
 END SUBROUTINE hello
