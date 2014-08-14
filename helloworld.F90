22625f78bebbbae61a51723a8b3a9fe89dd75ac1
jayeshkrishna <jayesh@mcs.anl.gov>
2014-08-14 13:45:29 -0500

    Adding comm and rank hello routines and tests
    Adding some simple routines in library helloworld - one accepts a
    communicator and prints hello and the second one accepts a rank
    and prints hello. Also adding pFUnit tests for these routines

--- helloworld.F90
+++ helloworld.F90
@@ -11,3 +11,29 @@ SUBROUTINE hello()
   PRINT *, "Hello world from ", rank, " of ", sz, " processes"
 !  call MPI_FINALIZE(ierr)
 END SUBROUTINE hello
+
+SUBROUTINE rank_hello(rank)
+  USE mpi
+
+  INTEGER, INTENT(IN) :: rank
+  INTEGER :: sz
+  INTEGER :: ierr
+
+  call MPI_COMM_SIZE(MPI_COMM_WORLD, sz, ierr) 
+
+  PRINT *, "Hello world from ", rank, " of ", sz, " processes"
+END SUBROUTINE rank_hello
+
+SUBROUTINE comm_hello(comm)
+  USE mpi
+  
+  INTEGER, INTENT(IN) :: comm
+  INTEGER :: sz, rank
+  INTEGER :: ierr
+
+  call MPI_COMM_SIZE(comm, sz, ierr) 
+  call MPI_COMM_RANK(comm, rank, ierr)
+
+  PRINT *, "Hello world from ", rank, " of ", sz, " processes"
+END SUBROUTINE comm_hello
+
